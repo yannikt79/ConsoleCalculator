b61a38aa00d1682586664fecf767615418aef969
yannikt79 <yannik.tobler@kbw.ch>
2021-06-23 11:53:56 +0200

    implemented subtraction

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int summe(int summand1,int summand2){
         return summand1+summand2;
     }
+
+    public int difference(int number1,int number2){
+        return number1-number2;
+    }
 }
